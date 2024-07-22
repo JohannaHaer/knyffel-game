00a98bf71de0ab2f47af21f6d5d7a2cf73a53e03
JohannaHaer <johanna.haerdter@gmail.com>
2024-07-22 16:33:56 +0200

    toggleClick hinzugefügt

--- frontend/src/components/Diceroll.jsx
+++ frontend/src/components/Diceroll.jsx
@@ -3,17 +3,27 @@ import { mainContext } from '../context/mainProvider'
 
 
 const Diceroll = () => {
-const {diceArray, setDiceArray} = useContext(mainContext)
-console.log(diceArray);
+  const {diceArray, setDiceArray, clicked1, setClicked1, clicked2, setClicked2, clicked3, setClicked3, clicked4, setClicked4, clicked5, setClicked5} = useContext(mainContext)
+  console.log(diceArray);
+
+  const toggleClick = (event, clicked, setClicked) => {
+    event.preventDefault()
+    if(!clicked) {
+      setClicked(true)
+    } else {
+      setClicked(false)
+    }
+  }
+
   return (
     <>
       <form>
         <div>
-          <button>{diceArray[0]}</button>
-          <button>{diceArray[1]}</button>
-          <button>{diceArray[2]}</button>
-          <button>{diceArray[3]}</button>
-          <button>{diceArray[4]}</button>
+          <button onClick={() => toggleClick(event, clicked1, setClicked1)}>{diceArray[0]}</button>
+          <button onClick={() => toggleClick(event, clicked2, setClicked2)}>{diceArray[1]}</button>
+          <button onClick={() => toggleClick(event, clicked3, setClicked3)}>{diceArray[2]}</button>
+          <button onClick={() => toggleClick(event, clicked4, setClicked4)}>{diceArray[3]}</button>
+          <button onClick={() => toggleClick(event, clicked5, setClicked5)}>{diceArray[4]}</button>
         </div>
         <button>Roll</button>
       </form>
--- frontend/src/context/mainProvider.jsx
+++ frontend/src/context/mainProvider.jsx
@@ -3,9 +3,14 @@ export const mainContext = createContext()
 
 const MainProvider = ({children}) => {
   const [diceArray, setDiceArray] = useState([1, 1, 1, 1, 1])
+  const [clicked1, setClicked1] = useState(false)
+  const [clicked2, setClicked2] = useState(false)
+  const [clicked3, setClicked3] = useState(false)
+  const [clicked4, setClicked4] = useState(false)
+  const [clicked5, setClicked5] = useState(false)
 
   return (
-    <mainContext.Provider value={{diceArray, setDiceArray}}>
+    <mainContext.Provider value={{diceArray, setDiceArray, clicked1, setClicked1, clicked2, setClicked2, clicked3, setClicked3, clicked4, setClicked4, clicked5, setClicked5}}>
         {children}
     </mainContext.Provider>
   )
