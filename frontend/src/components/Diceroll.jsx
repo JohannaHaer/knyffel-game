a1bbb745af994e021fe2be5432b4101f39d9d4b8
JohannaHaer <johanna.haerdter@gmail.com>
2024-07-29 14:30:17 +0200

    dice selected

--- frontend/src/components/Diceroll.jsx
+++ frontend/src/components/Diceroll.jsx
@@ -4,7 +4,6 @@ import { mainContext } from '../context/mainProvider'
 
 const Diceroll = () => {
   const {diceArray, setDiceArray, clicked1, setClicked1, clicked2, setClicked2, clicked3, setClicked3, clicked4, setClicked4, clicked5, setClicked5} = useContext(mainContext)
-  console.log(diceArray);
 
   const toggleClick = (event, clicked, setClicked) => {
     event.preventDefault()
@@ -15,17 +14,45 @@ const Diceroll = () => {
     }
   }
 
+  const rollDice = (e) => {
+    e.preventDefault()
+    let copyArray = [...diceArray]
+    if (!clicked1) {
+      let diceNumber = Math.round(Math.random() * 5 + 1)
+      copyArray[0] = diceNumber
+    }
+    if (!clicked2) {
+      let diceNumber = Math.round(Math.random() * 5 + 1)
+      copyArray[1] = diceNumber
+    }
+    if (!clicked3) {
+      let diceNumber = Math.round(Math.random() * 5 + 1)
+      copyArray[2] = diceNumber
+    }
+    if (!clicked4) {
+      let diceNumber = Math.round(Math.random() * 5 + 1)
+      copyArray[3] = diceNumber
+    }
+    if (!clicked5) {
+      let diceNumber = Math.round(Math.random() * 5 + 1)
+      copyArray[4] = diceNumber
+    }
+    setDiceArray(copyArray)
+  }
+
+  console.log(diceArray);
+
   return (
     <>
-      <form>
+      <form onSubmit={rollDice}>
         <div>
-          <button onClick={() => toggleClick(event, clicked1, setClicked1)}>{diceArray[0]}</button>
-          <button onClick={() => toggleClick(event, clicked2, setClicked2)}>{diceArray[1]}</button>
-          <button onClick={() => toggleClick(event, clicked3, setClicked3)}>{diceArray[2]}</button>
-          <button onClick={() => toggleClick(event, clicked4, setClicked4)}>{diceArray[3]}</button>
-          <button onClick={() => toggleClick(event, clicked5, setClicked5)}>{diceArray[4]}</button>
+          <div onClick={() => toggleClick(event, clicked1, setClicked1)}>{diceArray[0]}</div>
+          <div onClick={() => toggleClick(event, clicked2, setClicked2)}>{diceArray[1]}</div>
+          <div onClick={() => toggleClick(event, clicked3, setClicked3)}>{diceArray[2]}</div>
+          <div onClick={() => toggleClick(event, clicked4, setClicked4)}>{diceArray[3]}</div>
+          <div onClick={() => toggleClick(event, clicked5, setClicked5)}>{diceArray[4]}</div>
         </div>
-        <button>Roll</button>
+        <button type='submit'>Roll</button>
       </form>
     </>
   )
